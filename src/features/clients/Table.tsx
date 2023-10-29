50a28dba26a7007d32dd97616deb497437a727ad
Denys Novosad <denisnovosad3@gmail.com>
2023-10-29 18:41:41 +0200

    update endpoint

--- src/features/clients/Table.tsx
+++ src/features/clients/Table.tsx
@@ -1,24 +1,62 @@
 import { Client } from '../../types'
 import styles from './Table.module.css'
+import {activeId, updateClient} from "./clientsSlice";
+import {useDispatch, useSelector} from "react-redux";
+import {memo, useState} from "react";
 
 
 
 
 
-export default function Table({email,id,name,phone,username,_id}:Client) {
-    console.log(id)
+ function Table(client:Client) {
+    const dispatch=useDispatch();
+    //const isActive=useSelector(state=>state.clients.isActive)
+   const[isActive,setIsActive]=useState(false);
+   let [typeName,setTypeName]=useState(client.name);
+   let [typeEmail,setTypeEmail]=useState(client.email);
+  let [typePhone,setTypePhone]=useState(client.phone);
+  const activeId=useSelector(state=>state.clients.activeId)
+     const isAuth=useSelector(state=>state.auth.isAuth)
+    console.log(client)
+    /* name="Victor",
+     email='1234@ukr.net'
+     phone='222-222-222-222'*/
+    function handleSubmit(e){
+        e.preventDefault()
+       if(!typeName || !typeEmail || !typePhone) return
+        dispatch(updateClient(client._id,typeName,typeEmail,typePhone))
+      setIsActive(false)
+      dispatch({type:'clients/activeId',payload:null})
+    }
+    function handleActivate(){
+      if(activeId && client._id!==activeId) return
+       setIsActive(true)
+       setTypeName(client.name);
+       setTypeEmail(client.email);
+       setTypePhone(client.phone)
+        dispatch({type:'clients/activeId',payload:client._id})
+    }
     return (
-        <div className={styles.table}>
-
-
+        <div>
+            {isActive?
+                <form className={styles.tr} onSubmit={handleSubmit}>
+                    <input onChange={e=>setTypeName(e.target.value)} value={typeName} />
+                    <input onChange={e=>setTypeEmail(e.target.value)} value={typeEmail}/>
+                    <input onChange={e=>setTypePhone(e.target.value)} value={typePhone}/>
+                    <button >Submit</button>
+                </form>
+        :<div className={styles.table}>
             <div className={styles.tbody}>
             <div className={styles.tr}>
-                <div>{name}</div>
-                <div>{email}</div>
-                <div>{phone}</div>
-                <button >Edit data</button>
+                <div>{client.name}</div>
+                <div>{client.email}</div>
+                <div>{client.phone}</div>
+                {isAuth && <button disabled={activeId && activeId!==client._id} onClick={handleActivate}>Edit data</button>}
             </div>
             </div>
+        </div>}
         </div>
     )
 }
+
+export default memo(Table)
--- src/features/clients/clientsSlice.ts
+++ src/features/clients/clientsSlice.ts
@@ -3,7 +3,11 @@ import { Client } from "../../types";
 
 const initialState = {
   clients:[],
-  isLoading:false
+  isLoading:false,
+  activeId:null
+
+
+
 
 
 };
@@ -18,8 +22,42 @@ const clientsSlice = createSlice({
       state.clients = action.payload;
 
     },
+    updateClient: {
+      prepare(_id, name, email, phone) {
+        return {
+          payload: {_id, name, email, phone}
+        }
+      },
+      reducer(state, action) {
+
+        state.clients = state.clients.map(function (elem) {
+            console.log(action.payload)
+              if (action.payload._id === elem._id) {
+                console.log(action.payload._id === elem._id)
+                return ({
+                  _id: action.payload._id,
+                  id: action.payload.id,
+                  name: action.payload.name,
+                  email: action.payload.email,
+                  phone: action.payload.phone
+
+
+                })
+              } else{
+             //   console.log(elem)
+                return elem
+              }
+            }
+        )
+
+      }
+    },
+
     isLoading(state,action){
       state.isLoading=action.payload
+    },
+    activeId(state,action){
+      state.activeId=action.payload
     }
 
   },
@@ -44,7 +82,32 @@ export function getClients() {
     })
   };
 }
+export function updateClient(id,name,email,phone) {
+  console.log(id,name,email,phone)
+  return function (dispatch: any) {
+    dispatch({type:'clients/isLoading',payload:true})
+    fetch(`${host}/api/clients/${id}`,{
+      method:'put',
+      body:JSON.stringify({name,email,phone}),
+      headers: {
+        "content-type": "application/json"
+      }
+    })
+        .then((resp) => resp.json())
+        .then((data) => {
+          console.log(data.client);
 
-export const {isLoading} = clientsSlice.actions;
+          dispatch({ type: "clients/updateClient", payload:data.client});
+        })
+        .catch((error) => {
+          console.log(error);
+
+          alert(error.message);
+        }).finally(()=>{
+      dispatch({type:'clients/isLoading',payload:false})
+    })
+  };
+}
+export const {isLoading,activeId} = clientsSlice.actions;
 
 export default clientsSlice.reducer;
